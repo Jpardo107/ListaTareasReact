47e7a44e118b78696e41ef4c5b521595075371ba
Jaime Pardo <jdpm2014@gmail.com>
2022-02-05 19:22:13 -0300

    creacion del componente input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import FormularioTareas from './componentes/FormularioTareas';
 import Header from './componentes/Header';
+import React, {useState} from 'react';
+
 
 const App = () => {
+  const [tareas, cambiarTareas] =useState([])
+  console.log(tareas);
   return (
     <div className='contenedor'>
       <Header />
+      <FormularioTareas tareas={tareas} cambiarTareas={cambiarTareas}/>
     </div>
   );
 }
