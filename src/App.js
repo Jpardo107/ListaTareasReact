3ae92539c49b17ab131ab906d72da27d49db42fa
Jaime Pardo <jdpm2014@gmail.com>
2022-02-06 00:25:26 -0300

    Creacion de la lista de tareas

--- src/App.js
+++ src/App.js
@@ -2,15 +2,31 @@ import './App.css';
 import FormularioTareas from './componentes/FormularioTareas';
 import Header from './componentes/Header';
 import React, {useState} from 'react';
+import ListaTareas from './componentes/ListaTareas';
+import {v4 as uuidv4} from 'uuid'
 
 
 const App = () => {
-  const [tareas, cambiarTareas] =useState([])
+  const [tareas, cambiarTareas] =useState(
+    [
+      {
+        id:uuidv4(),
+        texto:'Lavarme las patas',
+        completada: false
+      },
+      {
+        id:uuidv4(),
+        texto:'Lavarme las bolas',
+        completada: false
+      }
+    ]
+  );
   console.log(tareas);
   return (
     <div className='contenedor'>
       <Header />
       <FormularioTareas tareas={tareas} cambiarTareas={cambiarTareas}/>
+      <ListaTareas tareas={tareas} />
     </div>
   );
 }
